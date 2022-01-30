ab8634410139f2f44aee4fbfb1ca829f08ca573e
Amir Jann Castaneda <amirj.castaneda@gmail.com>
2022-01-30 16:32:31 +0800

    Revised Landing page

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-function openExperienceTab(evt, cityName) {
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
+let tabLinks = document.getElementsByClassName("tabLinks");
+
+for (let i=0; i < tabLinks.length; i++) {
+	tabLinks[i].addEventListener('click', openExperienceTab);
+}
+
+function openExperienceTab(event) {
+	var i, tabLinks, tabContent;
+	tabLinks = document.getElementsByClassName("tabLinks");
+	tabContent = document.getElementsByClassName("tabContent");
+	for (i = 0; i < tabLinks.length; i++) {
+		tabLinks[i].className = tabLinks[i].className.replace(" active", "")
 	}
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	for (i = 0; i < tabLinks.length; i++) {
+		tabContent[i].className = tabContent[i].className.replace(" active", "")
 	}
-	document.getElementById(cityName).style.display = "block";
-	evt.currentTarget.className += " active";
+
+	document.querySelector(`#${event.target.id}`).classList.add("active");
+	document.querySelector(`#${event.target.id}Content`).classList.add("active");
 }
 
 function experienceNavHamburger() {
